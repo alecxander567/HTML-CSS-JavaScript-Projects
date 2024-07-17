f83eaf076d8b65e6fc976d0fd7182c6bbf16e02c
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-07-17 10:37:58 +0800

    Add files via upload

---
+++ Year Birth Generator/script.js
@@ -0,0 +1,8 @@
+let yearCont = document.getElementById('yearCont');
+        let age = document.getElementById('age');
+        const enterAge = document.getElementById('enterAge');
+
+        enterAge.addEventListener('click', function(){
+            let year = new Date();
+            yearCont.innerText =  parseInt(year.getFullYear()) - parseInt(age.value);
+        });
