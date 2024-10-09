cbf3f8cf160eb835caee028a567bb6c575639df6
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-10-09 11:57:40 +0800

    Add files via upload

---
+++ tic tac toe/tictactoe.js
@@ -0,0 +1,124 @@
+
+    const player1 = document.getElementById("playerX");
+    const player2 = document.getElementById("playerO");
+    const box1 = document.getElementById("box1");
+    const box2 = document.getElementById("box2");
+    const box3 = document.getElementById("box3");
+    const box4 = document.getElementById("box4");
+    const box5 = document.getElementById("box5");
+    const box6 = document.getElementById("box6");
+    const box7 = document.getElementById("box7");
+    const box8 = document.getElementById("box8");
+    const box9 = document.getElementById("box9");
+
+    player1.addEventListener("click", () => {
+        box1.addEventListener("click", () =>{
+            box1.innerText = "X";         
+        });
+    });
+
+    player1.addEventListener("click", () =>{
+        box2.addEventListener("click", () =>{
+                box2.innerText = "X";           
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box3.addEventListener("click", () =>{
+                box3.innerText = "X";              
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box4.addEventListener("click", () =>{
+            box4.innerText = "X";            
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box5.addEventListener("click", () =>{
+            box5.innerText = "X";         
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box6.addEventListener("click", () =>{
+            box6.innerText = "X";          
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box7.addEventListener("click", () =>{
+            box7.innerText = "X";            
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box8.addEventListener("click", () =>{
+            box8.innerText = "X";           
+        });
+    });
+
+    player1.addEventListener("click", () => {
+        box9.addEventListener("click", () =>{
+            box9.innerText = "X";            
+        });
+    });
+
+
+    player2.addEventListener("click", () => {
+        box1.addEventListener("click", () =>{
+            box1.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () =>{
+        box2.addEventListener("click", () =>{
+            box2.innerText = "O";            
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box3.addEventListener("click", () =>{
+            box3.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box4.addEventListener("click", () =>{
+            box4.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box5.addEventListener("click", () =>{
+            box5.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box6.addEventListener("click", () =>{
+            box6.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box7.addEventListener("click", () =>{
+            box7.innerText = "O";            
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box8.addEventListener("click", () =>{
+            box8.innerText = "O";           
+        });
+    });
+
+    player2.addEventListener("click", () => {
+        box9.addEventListener("click", () =>{
+            box9.innerText = "O";           
+        });
+    });
+
+
+   
