dffc19d0793753409bba537fb9f39e6025eb916d
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-10-15 17:39:51 +0800

    Add files via upload

---
+++ Calendar/script.js
@@ -0,0 +1,13 @@
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+let day = document.getElementById('day');
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let newYear = new Date();
+let newMonth = new Date();
+let newDay = new Date();
+
+year.innerText = newYear.getFullYear();
+month.innerText = months[newMonth.getMonth()];
+day.innerText = newDay.getDate();
