e367fdc2468686f6656b4055ee9b469be3869865
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-12-31 15:26:42 +0800

    Update script.js

--- Random Password Generator/script.js
+++ Random Password Generator/script.js
@@ -2,6 +2,19 @@ let pass = document.getElementById('pass');
 let btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
-    const passwords = ['eqgeu123', 'dhuqw#wie', 'duwgeuwe', '1y3gwekqi', 'iheyqe1'];
+    const passwords = [
+        'eqgeu123',
+        'dhuqw#wie',
+        'duwgeuwe',
+        '1y3gwekqi',
+        'iheyqe1',
+        'uefhwgewg123',
+        'fhwuefwef',
+        'dhwufefef56',
+        '45eit23*eihw',
+        'ihegih4g3',
+        'wetwit4ut'
+        ];
+        
     pass.innerHTML = passwords[Math.floor(Math.random() * passwords.length)];
 });
