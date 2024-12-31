8faed4bc893ea0a572f9f7fc6d76368610d97570
Mister Beans <162329514+alecxander567@users.noreply.github.com>
2024-12-31 15:28:46 +0800

    Update script.js

--- Calendar/script.js
+++ Calendar/script.js
@@ -2,7 +2,19 @@ let month = document.getElementById('month');
 let year = document.getElementById('year');
 let day = document.getElementById('day');
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = ["January",
+                "February",
+                "March", 
+                "April",
+                "May",
+                "June", 
+                "July",
+                "August",
+                "September", 
+                "October",
+                "November",
+                "December"
+               ];
 
 let newYear = new Date();
 let newMonth = new Date();
